b9eb9c76c037afc17f97e377c30017f8331fe823
PatriciaArnedo <paaarnedo@gmail.com>
2020-10-09 11:31:29 -0400

    Completed with Javaughn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, status="pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if  @status == "pending" && sender.balance > amount && valid?
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    elsif sender.balance < amount || !valid?
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+      # elsif status = "closed"
+      #   @status = 'rejected'
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      @status = "reversed"
+    end
+  end
+
 end
+
+
